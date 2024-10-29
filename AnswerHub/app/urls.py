76ec37359e499afd7e6505655eb17d913e3338df
kms-qwe <mishakr8987@gmail.com>
2024-10-29 23:43:59 +0300

    add url processor

---
+++ AnswerHub/app/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # Главная страница и списки вопросов
+    path('', views.home, name='home'),
+    path('hot/', views.hot, name='hot_questions'),
+    path('tag/<str:tag_name>/', views.tag, name='tag_questions'),
+    
+    # Работа с вопросами
+    path('question/<int:question_id>/', views.question, name='question'),
+    path('ask/', views.ask, name='new_question'),
+    
+    # Аутентификация
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+   
+   # Settings
+   path('settings/', views.settings, name='settings')
+
+]
